cb5dfb3b5282e997961c86ef56a46a23cf07e7d2
Yerasyl04 <91834677+Yerasyl04@users.noreply.github.com>
2023-11-21 18:13:54 +0600

    Delete v/Search-Filter-React directory

--- v/Search-Filter-React/src/Details.js
+++
@@ -1,8 +0,0 @@
-// Details.js
-import React from 'react';
-
-function Details() {
-  // Your Details component code here
-}
-
-export default Details;
--- v/Search-Filter-React/src/Home.js
+++
@@ -1,8 +0,0 @@
-// Home.js
-import React from 'react';
-
-function Home({ data, searchTerm, selectedYear, setSearchTerm, setSelectedYear }) {
-  // Your existing Home component code here
-}
-
-export default Home;
